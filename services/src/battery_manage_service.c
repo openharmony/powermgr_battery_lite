3ad3f270b3aaa1a312daf9f39b80e2cc8b524603
toutes <zhaoruiyuan2@huawei.com>
2021-07-21 14:40:05 +0800

    屏蔽日志打印
    Signed-off-by: toutes <zhaoruiyuan2@huawei.com>

--- services/src/battery_manage_service.c
+++ services/src/battery_manage_service.c
@@ -49,7 +49,7 @@ static BatteryService *BatteryGetInstance(void);
 static BOOL Initialize(Service *service, Identity identity)
 {
 
-    POWER_HILOGI("BatteryInitialize begin");
+    // POWER_HILOGI("BatteryInitialize begin");
     BatteryService *batterService = BatteryGetInstance();
     batterService->identity = identity;
     
@@ -59,20 +59,20 @@ static BOOL Initialize(Service *service, Identity identity)
 
 static const char *BatteryGetName(Service *service)
 {
-    POWER_HILOGI("BatteryGetName begin");
+    // POWER_HILOGI("BatteryGetName begin");
     return BATTERY_SERVICE;
 }
 
 void UpdateBatteryMsg(BatInfo* battery)
 {
-    POWER_HILOGI("UpdateBatteryMsg() start.");
+    // POWER_HILOGI("UpdateBatteryMsg() start.");
     g_batteryDevice = NewBatterInterfaceInstance();
     if (g_batteryDevice == NULL) {
-        POWER_HILOGI("UpdateBatteryMsg() error.");
+        // POWER_HILOGI("UpdateBatteryMsg() error.");
         return;
     }
     g_batteryDevice->UpdateBatInfo(battery);
-    POWER_HILOGI("UpdateBatteryMsg():succeed");
+    // POWER_HILOGI("UpdateBatteryMsg():succeed");
     return;
 }
 
@@ -90,12 +90,12 @@ static BOOL MessageHandle(Service *service, Request *request)
         default:
             break;
     }
-    POWER_HILOGI("BatteryMessageHandle begin");
+    // POWER_HILOGI("BatteryMessageHandle begin");
     return true;
 }
 static TaskConfig GetTaskConfig(Service *service)
 {
-    POWER_HILOGI("BatteryGetTaskConfig begin");
+    // POWER_HILOGI("BatteryGetTaskConfig begin");
     TaskConfig config = {LEVEL_HIGH, PRI_BELOW_NORMAL, BATTERY_STACK_SIZE, BATTERY_QUEUE_SIZE, SHARED_TASK};
     return config;
 }
@@ -130,28 +130,28 @@ static void BatteryFeatureTimeerInit(void)
     {
         time_  =  PowerMgrCreateTimer(DEFAULT_INTERVAL_MSECS, DEFAULT_INTERVAL_MSECS , BatteryMonitorHandle);
         if (time_ == NULL) {
-            POWER_HILOGI("BatteryFeatureTimeerInit failed.");
+            // POWER_HILOGI("BatteryFeatureTimeerInit failed.");
             return;
         }
         if (PowerMgrStartTimer(time_, NULL) == FALSE) {
 
-            POWER_HILOGI("Failed to start timer.");
+            // POWER_HILOGI("Failed to start timer.");
             return;
         }
 
-        POWER_HILOGI("BatteryFeatureTimeerInit()...ok.");
+        // POWER_HILOGI("BatteryFeatureTimeerInit()...ok.");
     }
 }
 
 static void Init(void)
 {
-    POWER_HILOGI("Succeed to init battery manager service begin");
+    // POWER_HILOGI("Succeed to init battery manager service begin");
     BOOL result = SAMGR_GetInstance()->RegisterService((Service *)&g_batteryService);
     if (result == FALSE) {
-        POWER_HILOGI("RegisterService failed.");
+        // POWER_HILOGI("RegisterService failed.");
         return;
     }
-    POWER_HILOGI("Succeed to init battery manager service");
-    POWER_HILOGI("Succeed to init battery manager service end");
+    // POWER_HILOGI("Succeed to init battery manager service");
+    // POWER_HILOGI("Succeed to init battery manager service end");
 }
 SYSEX_SERVICE_INIT(Init);
