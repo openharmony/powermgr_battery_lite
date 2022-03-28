e95969d451a36f22e638be6006f3c0b7ba2aed21
toutes <zhaoruiyuan2@huawei.com>
2022-03-27 23:19:43 -0700

    fix:Modify file permission information specification
    Signed-off-by: toutes <zhaoruiyuan2@huawei.com>

--- frameworks/js/builtin/src/battery_impl.c
+++ frameworks/js/builtin/src/battery_impl.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Copyright (c) 2021-2022 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -20,26 +20,32 @@ int32_t GetBatSocImpl(void)
 {
     return GetBatSoc();
 }
+
 BatteryChargeState GetChargingStatusImpl(void)
 {
     return GetChargingStatus();
 }
+
 BatteryHealthState GetHealthStatusImpl(void)
 {
     return GetHealthStatus();
 }
+
 BatteryPluggedType GetPluggedTypeImpl(void)
 {
     return GetPluggedType();
 }
+
 int32_t GetBatVoltageImpl(void)
 {
     return GetBatVoltage();
 }
+
 char *GetBatTechnologyImpl(void)
 {
     return GetBatTechnology();
 }
+
 int32_t GetBatTemperatureImpl(void)
 {
     return GetBatTemperature();
